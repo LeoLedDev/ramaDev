ec63b8b7be81312bdd1b3a8d9b9d356054399db4
Leonel Ledesma <leonelledesma.dev@outlook.com>
2023-08-18 12:36:11 -0300

    se crea estructura basica

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+const obtenerUsuarios = () => {
+    let resultado = JSON.parse(localStorage.getItem('usuarios'))
+    return resultado
+}
+
+
+const editarUsuario = (id) => {
+    return id
+}
+
+export {
+    obtenerUsuarios,
+    editarUsuario
+}
